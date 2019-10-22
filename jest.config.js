55508e1a82e2e222dc9735bf8ec6b3fb1cf52375
Jackes David <jackesdavid@hotmail.com>
2019-10-22 08:56:40 -0400

    add test configs to use babel or typescript

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    testPathIgnorePatterns: ['/node_modules/', '/cypress/'],
-    transform: {
-      '^.+\\.svelte$': 'svelte-test/transform',
-      '^.+\\.js$': 'babel-jest',
-    },
-    globals: {
-      svelte: {
-        
-        compilerOptions: {
-          accessors: true,
-        },
+  testPathIgnorePatterns: ['/node_modules/', '/cypress/'],
+  transform: {
+    '^.+\\.svelte$': 'svelte-test/transform',
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'svelte', 'ts'],
+  globals: {
+    svelte: {
+      compilerOptions: {
+        accessors: true,
       },
     },
-  };
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+};
