4bee383de1dbfc99f3021130fe8414095007e19a
Leandro AMARAL <le.amaral@outlook.com>
2019-11-20 12:41:35 -0400

    wc-194 - Add sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,2 @@
-export { default as Home } from './home.svelte';
 export { default as Button } from './button.page.svelte';
 export { default as Checkbox } from './checkbox.page.svelte';
--- src/shell.svelte
+++ src/shell.svelte
@@ -6,6 +6,11 @@
     '/components/button': page.Button,
     '/components/checkbox': page.Checkbox,
   };
+
+  const urlList = Object.keys(routes).map(item => ({
+    name: item.replace(/\/components\//, ''),
+    path: item,
+  }));
 </script>
 
 <style>
@@ -16,10 +21,8 @@
   :global(html, body) {
     width: 100%;
     height: 100%;
+    overflow: hidden;
   }
-
-  @import './reset.css';
-  
 
   .shell {
     display: flex;
@@ -30,15 +33,35 @@
     height: 100%;
     width: 20%;
     background-color: #cecece;
+    overflow-y: auto;
   }
-  
+
+  .list li {
+    margin: 0;
+    padding: 5px;
+  }
+
+  .list a {
+    text-transform: capitalize;
+    text-decoration: none;
+    outline: none;
+    color: rgba(0, 0, 0, 0.65);
+  }
+
+  .list a:hover {
+    font-weight: 800;
+  }
+
+  @import './reset.css';
   @import './pages/style.css';
 </style>
 
 <div class="shell">
   <ul class="list">
-    {#each Object.keys(routes) as route}
-      <li><a href="#{route}">{route}</a></li>
+    {#each urlList as url}
+      <li>
+        <a href="#{url.path}">{url.name}</a>
+      </li>
     {/each}
   </ul>
   <Router {routes} />
