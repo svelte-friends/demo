1dac2564a0d7406569ecb503e1ef851b8b119ecc
Marco Neves <marcobneves@gmail.com>
2019-11-21 18:16:21 -0400

    Change - Page base  component

--- src/pages/components/page.svelte
+++ src/pages/components/page.svelte
@@ -1,20 +1,3 @@
-<script>
-  let codeDemoBasic = `
-   <CheckBox />
-   <CheckBox label="Checkbox" checked />
-  `;
-
-  let codeDemoColor = `
-   <CheckBox label="Checkbox" color="red" />
-   <CheckBox label="Checkbox" checked color="red" />
-  `;
-
-  let codeDemoDisabled = `
-   <CheckBox label="Disabled" checked disabled />
-   <CheckBox label="Disabled" disabled />
-  `;
-</script>
-
 <style>
   .content {
     flex-direction: column;
