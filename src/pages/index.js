a1472d2b0c8d0a245f52e773ab813b1c35537806
Leandro AMARAL <le.amaral@outlook.com>
2019-12-12 16:09:28 -0400

    Update RadioButton specification

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ export { default as Button } from './button.page.svelte';
 export { default as Checkbox } from './checkbox.page.svelte';
 export { default as Switch } from './switch.page.svelte';
 export { default as ProgressRing } from './progressRing.page.svelte';
-export { default as Radio } from './radio.page.svelte';
+export { default as RadioButton } from './radioButton.page.svelte';
 export { default as ProgressBar } from './progressBar.page.svelte';
 export { default as DotLabel } from './dotLabel.page.svelte';
 export { default as Pagination } from './pagination.page.svelte';
--- src/pages/radioButton.page.svelte
+++ src/pages/radioButton.page.svelte
@@ -7,25 +7,44 @@
   import Table from './components/table.svelte';
   import Text from './components/text.svelte';
 
-  let codeDemoBasic = `
+  let basicCode = `
     <RadioButton />
     <RadioButton checked />
     <RadioButton label="RadioButton" />
     <RadioButton label="RadioButton" checked />
   `;
 
-  let codeDemoColor = `
+  let colorCode = `
     <RadioButton color="red" />
     <RadioButton color="green" checked />
     <RadioButton label="RadioButton" color="grey" checked />
     <RadioButton label="RadioButton" color="red" checked />
   `;
 
-  let codeDemoDisabled = `
+  let disabledCode = `
     <RadioButton color="red" disabled />
     <RadioButton color="green" checked disabled />
     <RadioButton label="RadioButton" color="grey" checked disabled />
     <RadioButton label="RadioButton" color="red" checked disabled />
+  `;
+
+  let groupedCode = `
+    <script>
+      let colors = ['Blue', 'Yellow', 'Red', 'Black'];
+      let selected = '';
+      function onChange(evt) {
+        selected = evt.target.value;
+      }
+    <\/script>
+    <span>Selected: {selected}</span>
+    {#each colors as color}
+      <RadioButton
+        name="colors"
+        on:change={onChange}
+        {color}
+        label={color}
+        value={color} />
+    {/each}
   `;
 
   let thead = [
@@ -37,52 +56,65 @@
   ];
 
   let tbody = [
-    ['name', 'We use to define the name.', 'String', 'true', 'undefined'],
-    [
-      'value',
-      'We use to define a value.',
-      'String / integer',
-      'true',
-      'undefined',
-    ],
-    ['label', 'We use to define a text.', 'String', 'true', '---'],
-    ['checked', 'We use to define if checked.', 'Boolean', 'true', 'false'],
-    ['color', 'We use to define a color.', 'String', 'true', '#624695'],
-    ['disabled', 'We use to define if disabled.', 'Boolean', 'true', 'false'],
+    ['name', 'We use to define the name.', 'String', 'no', '--'],
+    ['value', 'We use to define a value.', 'String', 'no', '--'],
+    ['label', 'We use to define a text.', 'String', 'no', '--'],
+    ['checked', 'We use to define if checked.', 'Boolean', 'yes', 'false'],
+    ['color', 'We use to define a color.', 'String', 'yes', '#624695'],
+    ['disabled', 'We use to define if disabled.', 'Boolean', 'yes', 'false'],
   ];
+
+  let colors = ['Blue', 'Yellow', 'Red', 'Black'];
+  let selected = '';
+  function onChange(evt) {
+    selected = evt.target.value;
+  }
 </script>
 
 <BasePage>
-  <Title size="h2" text="Radio" />
+  <Title size="h2" text="RadioButton" />
   <Text>Provide a radio button for individual selection.</Text>
   <Title size="h3" text="Attributes" />
   <Table {thead} {tbody} />
 
-  <Title size="h5" text="Basic" />
+  <Title size="h4" text="Basic" />
   <Example>
     <RadioButton />
     <RadioButton checked />
     <RadioButton label="RadioButton" />
     <RadioButton label="RadioButton" checked />
   </Example>
-  <Code code={codeDemoBasic} />
+  <Code code={basicCode} />
 
-  <Title size="h5" text="Colors" />
+  <Title size="h4" text="Colors" />
   <Example>
     <RadioButton color="red" />
     <RadioButton color="green" checked />
     <RadioButton label="RadioButton" color="grey" checked />
     <RadioButton label="RadioButton" color="red" checked />
   </Example>
-  <Code code={codeDemoColor} />
+  <Code code={colorCode} />
 
-  <Title size="h5" text="Disabled" />
+  <Title size="h4" text="Disabled" />
   <Example>
     <RadioButton color="red" disabled />
     <RadioButton color="green" checked disabled />
     <RadioButton label="RadioButton" color="grey" checked disabled />
     <RadioButton label="RadioButton" color="red" checked disabled />
   </Example>
-  <Code code={codeDemoDisabled} />
+  <Code code={disabledCode} />
 
+  <Title size="h4" text="Grouped" />
+  <div class="group">
+    <span>Selected: {selected}</span>
+    {#each colors as color}
+      <RadioButton
+        name="colors"
+        on:change={onChange}
+        {color}
+        label={color}
+        value={color} />
+    {/each}
+  </div>
+  <Code code={groupedCode} />
 </BasePage>
